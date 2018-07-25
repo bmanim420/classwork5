2e8e7f2811b829e5e812526bae970419b4cfb6ef
Anim Mostafa <bm.anim420@gmail.com>
2018-07-25 04:41:04 +0000

    classwork8

--- server.js
+++ server.js
@@ -5,7 +5,27 @@ var http = require('http');
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
   var server = http.Server(app);
+  var mongo = require('mongodb');
+  //for local
+  //var db, uri = "mongodb://localhost:27817";
+  //for c9
+  var db, uri = "mongodb://"+process.env.IP+":27017";
   
+  mongo.MongoClient.connect(uri,
+          {useNewUrlParser:true},
+          function(err, client){
+            if(err){
+              console.log('Could not connect to MongoDB');
+            }else{
+              db = client.db('node-cw8');
+            }
+          });
+  
+  var save = function(form_data){
+    db.createCollection('users', function(err, collection){});
+    var collection = db.collection('users');
+    collection.save(form_data);
+  }
   
   app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
@@ -13,6 +33,8 @@ var http = require('http');
   
   app.post('/submit_user', function(req, res){
     console.log(req.body);
+    save(req.body);
+    res.status(200);
   });
   
   app.get('/about', function(req, res){
