cf97902b374e025b24ff62b2d6959075a7384224
Anim Mostafa <bm.anim420@gmail.com>
2018-07-30 04:29:43 +0000

    classwork9

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ var http = require('http');
   //var db, uri = "mongodb://localhost:27817";
   //for c9
   var db, uri = "mongodb://"+process.env.IP+":27017";
+  const mongoose = require('mongoose');
+  mongoose.connect("mongodb://localhost:27017/node-cw8");
+  
+  
+  mongoose.connection.on('error', function(){
+    console.log('Could not connect to mongodb');
+  });
+  
+  var userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: "Name is required"
+    },
+      email: String
+  });
+  var User  = mongoose.model('User', userSchema);
   
   mongo.MongoClient.connect(uri,
           {useNewUrlParser:true},
@@ -20,21 +36,29 @@ var http = require('http');
               db = client.db('node-cw8');
             }
           });
-  
+  /*
   var save = function(form_data){
     db.createCollection('users', function(err, collection){});
     var collection = db.collection('users');
     collection.save(form_data);
   }
-  
+  */
   app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
   });
   
   app.post('/submit_user', function(req, res){
     console.log(req.body);
-    save(req.body);
-    res.status(200);
+    var new_user = new User(req.body);
+    new_user.save(function(err, data){
+      if(err){
+        return res.status(400)
+        .json({message:"Couldn't save user"})
+      }
+      res.status(200).json(data);
+    });
+    //save(req.body);
+    
   });
   
   app.get('/about', function(req, res){
